df6aaae44c98189c4167c00bfd29ae148bb7d58b
Ian Grindelwald <164880317+LordGrindelwald@users.noreply.github.com>
2024-04-14 20:17:44 +0530

    Update ReadWN.lua

--- src/en/ReadWN.lua
+++ src/en/ReadWN.lua
@@ -59,7 +59,7 @@ local GENRES = {
 }
 
 return Require("ReadWN")("https://www.wuxiabox.com", {
-	id = 1308639964,
+	id = 8900091,
 	name = "I ReadWN™",
 	shrinkURLNovel = "^.-wuxiabox%.com",
 	hasCloudFlare = true,
